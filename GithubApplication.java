c47af7a927425307a882cad7ba669a118e427691
ifavasu <ifavila@asu.edu>
2023-06-23 20:50:25 -0500

    merged branch intended to be merged

--- GithubApplication.java
+++ GithubApplication.java
@@ -1,5 +1,7 @@
 class GithubApplication{
     public static void main(String[] args){
         System.out.println("Testing for github!");
+        int a = AdditionExample.addNums(1, 2);
+        System.out.println(a);
     }
 }
